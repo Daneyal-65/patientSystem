0a88352125fc235dff87b82c836991e1ef10c61f
daneyal <saifixzd@gmail.com>
2024-10-25 22:41:08 +0530

    url

--- client/src/auth/config.js
+++ client/src/auth/config.js
@@ -1,3 +1,5 @@
 // export const authUrl = "http://localhost:8080/auth";
 // export const authUrl = "https://book-my-show-server-ezk9.onrender.com/auth";
-export const authApi = "http://localhost:8080/api/auth";
+// export const authApi = "http://localhost:8080/api/auth";
+export const authApi = "https://patient-system-6plf.vercel.app/api/auth";
+// https://patient-system-6plf.vercel.app/api/auth
--- client/src/constant/url.js
+++ client/src/constant/url.js
@@ -1,4 +1,6 @@
-const appointmentApi = "http://localhost:8080/api/appointments";
+// const appointmentApi = "http://localhost:8080/api/appointments";
+const appointmentApi =
+  "https://patient-system-6plf.vercel.app/api/appointments";
 export { appointmentApi };
 
 // getset@gmail.com
